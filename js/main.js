796d80ffeb0b68e1bc681b8133d1f745e97cae7b
Sergey Maksimenko <64726770+sergey-max-vin@users.noreply.github.com>
2022-06-28 15:40:41 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+const navBtnImg = document.querySelector('#nav-btn-img');
+
+navBtn.onclick = () => {
+    if (nav.classList.toggle('open')) {
+    navBtnImg.src = "./image/icons/nav-close.svg";
+    } else {
+        navBtnImg.src = "./image/icons/nav-open.svg";
+    }
+}
+
+AOS.init({
+    //disable: 'phone' /*Отключение на мобильных устройствах*/
+    //once: true /*Подключаем эту строку только если отвлекает анимация при скроле, и в этом случае анимация будет только один раз*/
+});
